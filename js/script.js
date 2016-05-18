60e1fdf8de7f897e69f3893efd39f09c5c349418
Ezumezu <aezumezu@gmail.com>
2016-05-18 17:13:22 +0100

    basic app layout

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var firstInput;
+var secondInput;
+
+var addNumbers = function(num1, num2){
+	//function definition
+};
+
+var subtractNumbers = function(num1, num2){
+	//function definition	
+};
+
+var multiplyNumbers = function(num1, num2){
+	//function definition
+};
+
+var divideNumbers = function(num1, num2){
+	//function definition
+};
+
+var clearScreen = function{
+	//function definition
+};
+
+var getclick = function(){
+	//function definition	
+}
